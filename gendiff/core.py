3929f7e8ed8472f235b724cbadbe9def3703f194
DenisShutov <shtvdenis93@yandex.ru>
2025-11-27 11:56:59 +0500

    Ф-ия generate_diff

---
+++ gendiff/core.py
@@ -0,0 +1,24 @@
+import json
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+    data3 = data1 | data2
+    data_sort = dict(sorted(data3.items()))
+    data_diff = {}
+
+    for key, value in data_sort.items():
+        if key in data1 and key not in data2:
+            data_diff[f'- {key}'] = value
+        if key in data1 and key in data2:
+            if data1[key] != data2[key]:
+                data_diff[f'- {key}'] = data1[key]
+                data_diff[f'+ {key}'] = data2[key]
+            else:
+                data_diff[f'  {key}'] = value
+        if key not in data1 and key in data2:
+            data_diff[f'+ {key}'] = value
+    data_string = json.dumps(data_diff, indent=2)
+
+    return data_string
