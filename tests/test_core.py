28c025a31aee58deb447059d5d1155f658eb3954
DenisShutov <shtvdenis93@yandex.ru>
2025-12-03 12:55:49 +0500

    тесты для сравенения YAML файлов

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,3 +7,19 @@ def test_generate():
     assert isinstance(result, str)
     assert result[0] == '{'
     assert result[-1] == '}'
+
+
+def test_generate_yaml():
+    result = generate_diff('file1.yaml', 'file2.yaml')
+    assert len(result) > 0
+    assert isinstance(result, str)
+    assert result[0] == '{'
+    assert result[-1] == '}'
+    assert result == '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
