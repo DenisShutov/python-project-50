f189402fc6662a82e79c76251933e86806e39047
DenisShutov <shtvdenis93@yandex.ru>
2025-12-09 10:17:46 +0500

    Тесты дял форматтеров

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,9 @@
+import pytest
 from gendiff import generate_diff
 
 
 def test_generate():
-    result = generate_diff('file1.json', 'file2.json')
+    result = generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json')
     assert len(result) > 0
     assert isinstance(result, str)
     assert result[0] == '{'
@@ -10,7 +11,7 @@ def test_generate():
 
 
 def test_generate_yaml():
-    result = generate_diff('file1.yaml', 'file2.yaml')
+    result = generate_diff('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml')
     assert len(result) > 0
     assert isinstance(result, str)
     assert result[0] == '{'
@@ -25,5 +26,25 @@ def test_generate_yaml():
 }'''
 
 
+def read_file(path):
+    with open(path, 'r') as f:
+      return f.read()
+    
+@pytest.mark.parametrize('file3, file4, expected_file', [
+    ('tests/test_data/file3.json', 'tests/test_data/file4.json', 'tests/test_data/expected_result_stylish.txt'),
+    ('tests/test_data/file3.yaml', 'tests/test_data/file4.yaml', 'tests/test_data/expected_result_stylish.txt'),
+])
+def test_stylish(file3, file4, expected_file):
+    result = generate_diff(file3, file4, 'stylish')
+    expected = read_file(expected_file)
+    assert result.strip() == expected.strip()
 
 
+@pytest.mark.parametrize('file3, file4, expected_file', [
+    ('tests/test_data/file3.json', 'tests/test_data/file4.json', 'tests/test_data/expected_result_plain.txt'),
+    ('tests/test_data/file3.yaml', 'tests/test_data/file4.yaml', 'tests/test_data/expected_result_plain.txt'),
+])
+def test_plain(file3, file4, expected_file):
+    result = generate_diff(file3, file4, 'plain')
+    expected = read_file(expected_file)
+    assert result.strip() == expected.strip()
